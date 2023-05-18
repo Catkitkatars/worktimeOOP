32887af33a5e5ce07ba962b5ed5afbb7499db1c4
peseniy <catkitkat.ars@yandex.ru>
2023-05-18 23:16:25 +0400

    last commit

--- index.php
+++ index.php
@@ -43,33 +43,33 @@ class OrderCounter {
         if($this->orderDate->format('N') == 5 && ($this->orderDate->format('H') >= $this->workTime['finish'] || $this->timer < $this->workTimeHours)) {
             $skip += ($daysInWeek - $this->orderDate->format('N'));
         }
-        
-        return 'P'. $skip .'D';
+
+        return $this->orderDate->add(new DateInterval('P'. $skip .'D'));
     }
 
     public function getTime() 
     {   
         if($this->orderDate->format('N') >= 6) {
-            $this->orderDate->add(new DateInterval($this->skipDays()));
+            $this->skipDays();
             $this->orderDate->setTime($this->workTime['start'], 0, 0);
         }
  
         if($this->orderDate->format('H') >= $this->workTime['start'] && $this->orderDate->format('H') < $this->workTime['finish']) {
             $this->timer -= ($this->workTime['finish'] - $this->orderDate->format('H'));
-            $this->orderDate->add(new DateInterval($this->skipDays()));
+            $this->skipDays();
         }
 
         if($this->orderDate->format('H') < $this->workTime['start']) {
             $this->orderDate->setTime($this->workTime['start'], 0, 0);
 
         }elseif($this->orderDate->format('H') >= $this->workTime['finish']) {
-            $this->orderDate->add(new DateInterval($this->skipDays()));
+            $this->skipDays();
             $this->orderDate->setTime($this->workTime['start'], 0, 0);
         }
         
         while($this->timer >= $this->workTimeHours) {
             $this->timer -= $this->workTimeHours;
-            $this->orderDate->add(new DateInterval($this->skipDays()));
+            $this->skipDays();
         }
 
         $this->orderDate->setTime(
@@ -85,9 +85,9 @@ class OrderCounter {
 
 $cases = [
     'последний день февраля' => [['2023.02.28 01:23:28', [10, 19]], '2023.03.02 16:00:00'], // done
-    'чуть-чуть не хватило' => [['2023.05.03 13:00:01', [10, 19]], '2023.05.08 10:00:01'], // done
+    'чуть-чуть не хватило' => [['2023.05.03 13:00:01', [10, 19]], '2023.05.08 10:00:01'], // done 
     'заказ в выходной' => [['2023.05.06 13:00:01', [10, 19]], '2023.05.10 16:00:00'], // done
-    'ранняя пташка' => [['2023.05.15 09:00:00', [10, 19]], '2023.05.17 16:00:00'], // 
-    'супер пунктуальность' => [['2023.05.15 10:00:00', [10, 19]], '2023.05.17 16:00:00'], // 
-    'ошибка на единицу' => [['2023.05.15 19:00:00', [10, 19]], '2023.05.18 16:00:00'], // 
+    'ранняя пташка' => [['2023.05.15 09:00:00', [10, 19]], '2023.05.17 16:00:00'], // done
+    'супер пунктуальность' => [['2023.05.15 10:00:00', [10, 19]], '2023.05.17 16:00:00'], // done
+    'ошибка на единицу' => [['2023.05.15 19:00:00', [10, 19]], '2023.05.18 16:00:00'], // done
 ];
