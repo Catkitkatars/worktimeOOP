487ffc0239514ea446ffc5258869dae202e070d6
peseniy <catkitkat.ars@yandex.ru>
2023-05-18 18:24:12 +0400

    second commit

--- index.php
+++ index.php
@@ -22,40 +22,55 @@ class OrderCounter {
     public $workTime;
     public $timer;
 
+    private $workTimeHours;
+
     public function __construct($orderDate, $workTime, $timer)
     {
         $this->orderDate = $orderDate;
         $this->workTime = $workTime;
         $this->timer = $timer;
+        $this->workTimeHours = $this->workTime['finish'] - $this->workTime['start'];
+    }
+
+    private function skipDays() {
+        $skip = 1;
+        $daysInWeek = 7;
+
+        if($this->orderDate->format('N') == 6) {
+            $skip += ($daysInWeek - $this->orderDate->format('N'));
+        } 
+
+        if($this->orderDate->format('N') == 5 && ($this->orderDate->format('H') >= $this->workTime['finish'] || $this->timer < $this->workTimeHours)) {
+            $skip += ($daysInWeek - $this->orderDate->format('N'));
+        }
+        
+        return 'P'. $skip .'D';
     }
 
     public function getTime() 
     {   
-        $workTimeHours = $this->workTime['finish'] - $this->workTime['start'];
-
-        if($this->orderDate->format('H') >= $this->workTime['start'] && $this->orderDate->format('H') < $this->workTime['finish'] && $this->orderDate->format('N') != 6 && $this->orderDate->format('N') != 7) {
-            $this->timer -= ($this->workTime['finish'] - $this->orderDate->format('H'));
-            $this->orderDate->add(new DateInterval("P1D"));
-        }
-        if($this->orderDate->format('N') == 6 || $this->orderDate->format('N') == 7) {
-            $this->orderDate->add(new DateInterval("P1D"));
+        if($this->orderDate->format('N') >= 6) {
+            $this->orderDate->add(new DateInterval($this->skipDays()));
             $this->orderDate->setTime($this->workTime['start'], 0, 0);
         }
-        if($this->orderDate->format('H') >= $this->workTime['finish']) {
-            $this->orderDate->add(new DateInterval("P1D"));
+ 
+        if($this->orderDate->format('H') >= $this->workTime['start'] && $this->orderDate->format('H') < $this->workTime['finish']) {
+            $this->timer -= ($this->workTime['finish'] - $this->orderDate->format('H'));
+            $this->orderDate->add(new DateInterval($this->skipDays()));
         }
 
-        while($this->timer >= $workTimeHours) {
+        if($this->orderDate->format('H') < $this->workTime['start']) {
+            $this->orderDate->setTime($this->workTime['start'], 0, 0);
 
-            if($this->orderDate->format('N') == 6 || $this->orderDate->format('N') == 7) {
-                $this->orderDate->add(new DateInterval("P1D"));
-                continue; 
-            }
-            $this->timer -= $workTimeHours;
-            $this->orderDate->add(new DateInterval("P1D"));
+        }elseif($this->orderDate->format('H') >= $this->workTime['finish']) {
+            $this->orderDate->add(new DateInterval($this->skipDays()));
+            $this->orderDate->setTime($this->workTime['start'], 0, 0);
         }
-
-
+        
+        while($this->timer >= $this->workTimeHours) {
+            $this->timer -= $this->workTimeHours;
+            $this->orderDate->add(new DateInterval($this->skipDays()));
+        }
 
         $this->orderDate->setTime(
             ($this->workTime['start'] + $this->timer),
@@ -63,20 +78,16 @@ class OrderCounter {
             intval($this->orderDate->format('s'))
         );
 
-        while($this->orderDate->format('N') == 6 || $this->orderDate->format('N') == 7) {
-            $this->orderDate->add(new DateInterval("P1D"));
-        }
-
         return $this->orderDate;
     }
 }
 
 
 $cases = [
-    'последний день февраля' => [['2023.02.28 01:23:28', [10, 19]], '2023.03.02 16:23:28'], // done
+    'последний день февраля' => [['2023.02.28 01:23:28', [10, 19]], '2023.03.02 16:00:00'], // done
     'чуть-чуть не хватило' => [['2023.05.03 13:00:01', [10, 19]], '2023.05.08 10:00:01'], // done
     'заказ в выходной' => [['2023.05.06 13:00:01', [10, 19]], '2023.05.10 16:00:00'], // done
-    'ранняя пташка' => [['2023.05.15 09:00:00', [10, 19]], '2023.05.17 16:00:00'], // done
-    'супер пунктуальность' => [['2023.05.15 10:00:00', [10, 19]], '2023.05.17 16:00:00'], // done
+    'ранняя пташка' => [['2023.05.15 09:00:00', [10, 19]], '2023.05.17 16:00:00'], // 
+    'супер пунктуальность' => [['2023.05.15 10:00:00', [10, 19]], '2023.05.17 16:00:00'], // 
     'ошибка на единицу' => [['2023.05.15 19:00:00', [10, 19]], '2023.05.18 16:00:00'], // 
 ];
